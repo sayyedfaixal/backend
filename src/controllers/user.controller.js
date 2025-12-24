b852cf5547b0e923881c3217f0f1684020cd40a5
Faisal Sayed <sayedfaisal544@gmail.com>
2025-12-24 16:45:35 +0530

    fix: resolve logout MongoDB update error

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -169,16 +169,23 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-  await User.findByIdAndUpdate(req.user._id, {
-    $set: [
-      {
-        refreshToken: undefined,
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $unset: {
+        refreshToken: 1,
       },
-      {
-        new: true,
-      },
-    ],
-  });
+    },
+    {
+      new: true,
+    }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
   return res
     .status(200)
     .clearCookie("accessToken", options)
